81664bd516d6e830798ecc3c0ffb4019b1d7cae8
Scott Tesoriere <scott@keepflippin.com>
2010-09-15 07:29:24 -0400

    working

--- config/initializers/compass.rb
+++ config/initializers/compass.rb
@@ -1,7 +1,7 @@
 require 'compass'
 require 'compass/app_integration/rails'
-require "fileutils"
+#require "fileutils"
 
 Compass::AppIntegration::Rails.initialize!
-FileUtils.mkdir_p(Rails.root.join("tmp", "stylesheets", "compiled"))
-ActionController::Middleware.use(Rack::Static, :root => "tmp/", :urls => ["/stylesheets"])
+#FileUtils.mkdir_p(Rails.root.join("tmp", "stylesheets", "compiled"))
+#ActionController::Middleware.use(Rack::Static, :root => "tmp/", :urls => ["/stylesheets"])
