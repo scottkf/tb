d622fa44f29f6f9e2a76c195688eae5b8d2a7387
Scott Tesoriere <scott@keepflippin.com>
2010-09-15 07:52:21 -0400

    last time

--- config.ru
+++ config.ru
@@ -1,4 +1,9 @@
 # This file is used by Rack-based servers to start the application.
 
 require ::File.expand_path('../config/environment',  __FILE__)
+
+require 'sass/plugin/rack'
+use Sass::Plugin::Rack
+Sass::Plugin.options[:never_update] = true
+
 run A487teabagsCom::Application
--- config/initializers/compass.rb
+++ config/initializers/compass.rb
@@ -1,8 +1,4 @@
 require 'compass'
 require 'compass/app_integration/rails'
-#require "fileutils"
 
-Sass::Plugin.options[:never_update] = true
 Compass::AppIntegration::Rails.initialize!
-#FileUtils.mkdir_p(Rails.root.join("tmp", "stylesheets", "compiled"))
-#ActionController::Middleware.use(Rack::Static, :root => "tmp/", :urls => ["/stylesheets"])
