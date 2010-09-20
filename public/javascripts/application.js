2216deb7b8c97d7e2e77c8dcb140c7d6b9128251
Scott Tesoriere <scott@keepflippin.com>
2010-09-20 08:39:47 -0400

    adding ganalytics

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -12,16 +12,6 @@ $(document).ready(function() {
   });
   
   
-  //analytics
-  var _gaq = _gaq || [];
-  _gaq.push(['_setAccount', 'UA-6824117-5']);
-  _gaq.push(['_trackPageview']);
-
-  (function() {
-    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-  })();
 
   //if it exists, remove the flash[]
   jQuery.each(['.message', '.alert', '.notice'], function(index, value) {
