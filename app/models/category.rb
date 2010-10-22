7e4ed54f48f2b4b52af0d7c2cb8fd1fdddef65d4
Scott Tesoriere <scott@keepflippin.com>
2010-10-22 11:37:18 -0400

    removed data from migrations, forgot to redirect upon errors for category html create, fixed typo

--- app/models/category.rb
+++ app/models/category.rb
@@ -11,7 +11,7 @@ class Category < ActiveRecord::Base
   before_save :layout_exists
 
   validates_format_of :url, :with => /\A[a-zA-Z0-9]+\z/, :message => "cannot have special characters"
-  validates_length_of :url, :in => 3..10, :too_short => "must have atleast 3 characters", :too_long => "must have 10 or fewers characters"
+  validates_length_of :url, :in => 3..10, :too_short => "must have atleast 3 characters", :too_long => "must have 10 or fewer characters"
 
   private
   def layout_exists
